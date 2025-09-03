16977e5761640053d3f9fd7248ae9aaec0090153
bbw-tes <sabina.teleskumar@lernende.bbw.ch>
2025-09-03 16:08:53 +0200

    USer Modelklass is done

--- src/main/java/Model/Post.java
+++ src/main/java/Model/Post.java
@@ -1,2 +1,8 @@
 package bbw.sabina.Model;
 
+public class Post {
+    private int id;
+    private String title;
+    private String content;
+    private User user;
+}
--- src/main/java/Model/User.java
+++ src/main/java/Model/User.java
@@ -1,6 +1,46 @@
 package bbw.sabina.Model;
 
 public class User {
-
+    //Attribute
+    private int id;
+    private String username;
+    private String password;
+    private boolean role;    // true = Admin, false = normal User
 }
+
+public User(int id, String username, String password, String role ){
+    this.id = id;
+    this.username = username;
+    this.password = password;
+    this.role = role;
+}
+
+// Getter
+public int getId() {
+    return id;
+}
+
+public String getusername {
+    return username;
+}
+
+public String getpassword {
+    return password;
+}
+
+public boolean getRole {
+    return role;
+}
+
+// Setter
+public void setUsername(String username){
+    this.username = username;
+}
+
+public void password(String password) {
+    this.password = password;
+}
+
+
+
 
