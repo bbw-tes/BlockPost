b538e2d8de1e5cff25a8e758dc8e8ce3b3320515
bbw-tes <sabina.teleskumar@lernende.bbw.ch>
2025-09-10 17:50:13 +0200

    Projekt is now Java not Maven anymore. Main Test worked. | Now creating Services. STarting with UserService

---
+++ src/bbw/sabina/services/UserService.java
@@ -0,0 +1,22 @@
+package bbw.sabina.services;
+import bbw.sabina.models.User;
+import java.util.ArrayList;
+
+public class UserService {
+    // Methode erstellen: registerUser
+    public void registerUser(){
+        return user;
+    }
+    // Soll Methode registerUser überprüfen ob der username existiert
+
+    //Neues User-Object erzeugen
+    private User user = users;
+    //Objecte in Liste "Users" hinzufügen.
+    private ArrayList<User> users = new ArrayList<>();
+
+    //Erfolgsmeldung (optional)
+
+
+
+
+}
--- src/main/java/bbw/sabina/Main.java
+++
@@ -1,8 +0,0 @@
-package bbw.sabina;
-
-import bbw.sabina.Model.*;
-
-public static void main(String[]args){
-    User user1 = new User("John", "123", false );
-    Post post1 = new Post("OOP", "Ich kann es immer noch nicht", user1 )
-        }
